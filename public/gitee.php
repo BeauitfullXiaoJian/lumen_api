a98656cb8f4ec1864ce12a8bcfce1a9b08ab0c17
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2018-01-06 13:45:22 +0800

    add gitee webhook

---
+++ public/gitee.php
@@ -0,0 +1,13 @@
+<?php
+
+// 本地仓库路径
+$local = __DIR__;
+// 安全验证字符串，为空则不验证
+$secret = '123456789';
+
+$payload = file_get_contents('php://input');
+var_dump($payload);
+
+// echo shell_exec("git -C {$local}/.. pull 2>&1");
+// echo shell_exec('whoami');
+die("done " . date('Y-m-d H:i:s', time()));
